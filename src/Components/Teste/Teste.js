f125e2454954fccb5779371936b96edfd373637d
David Vanjão <35746587+davidvanjao@users.noreply.github.com>
2025-01-26 17:28:28 -0300

    Adiciona componente Teste

---
+++ src/Components/Teste/Teste.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import React  from 'react';
+import {FaUser, FaLock} from 'react-icons/fa';//biblioteca de icones
+
+import "./Teste.css";
+
+const Teste = () => {
+
+
+  return (
+    <div className="container">
+        <h1>Algum teste</h1>
+    </div>
+  )
+}
+
+export default Teste;
