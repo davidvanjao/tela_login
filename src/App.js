fe8f70b053a6097563b852a28129bdf037ef8b72
David Vanjão <35746587+davidvanjao@users.noreply.github.com>
2025-01-26 17:18:14 -0300

    teste de alteracao do branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Components/Login/Login';
+import Teste from './Components/Teste/Teste';
 
 function App() {
   return (
     
     <div className="App">
-      <Login/>
+      <Teste/>
     </div>
     
   );
